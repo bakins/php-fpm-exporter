1f6acc631f7a6c3bac3bdf03bb59a5518213116b
Andrew Howden <hello@andrewhowden.com>
2017-07-26 12:43:31 +0200

    Rename "max_active_processes to active_max_processes"
    Previous work defined the precedent of using the format
    ${DESCRIPTOR}_${BOUND}_${UNIT} with the use of "listen_queue_max_connections".
    This commit expands upon that renaming
    "max_active_processes" to "active_max_processes".
    
    Design Notes:
    
    is "active" a characteristic?
    
      Prometheus notes that labels should be used to delimit
      several metrics that express fundamentally the same
      construct, but expressed in terms of a given characteristic.
      One metric that qualifies in this regard is the "processes"
      set of metrics. They can be expressed as "idle" and "active",
      the sum of which becomes the total.
    
      However, refactoring this is beyond the scope of this work,
      and the author believes, beyond the scope of their skill.
    
    [1] - https://prometheus.io/docs/practices/naming/#labels

--- collector.go
+++ collector.go
@@ -54,7 +54,7 @@ func (e *Exporter) newCollector() *collector {
 		idleProcesses:      newFuncMetric("idle_processes", "Idle process count"),
 		activeProcesses:    newFuncMetric("active_processes", "Active process count"),
 		totalProcesses:     newFuncMetric("total_processes", "Total process count"),
-		maxActiveProcesses: newFuncMetric("max_active_processes", "Maximum active process count"),
+		maxActiveProcesses: newFuncMetric("active_max_processes", "Maximum active process count"),
 		maxChildrenReached: newFuncMetric("max_children_reached", "Number of times the process limit has been reached"),
 		slowRequests:       newFuncMetric("slow_requests", "Number of requests that exceed request_slowlog_timeout"),
 		scrapeFailures:     newFuncMetric("scrape_failures", "Number of errors while scraping php_fpm"),
