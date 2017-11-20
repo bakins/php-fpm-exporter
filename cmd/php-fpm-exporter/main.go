68f0384a089beabc8c80ba011e6fd4b9ae37c57d
eelmasllari <erion@tipi>
2017-11-20 01:14:33 +0100

    compiled with certs

--- cmd/php-fpm-exporter/main.go
+++ cmd/php-fpm-exporter/main.go
@@ -1,53 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"os"
+        "fmt"
+        "os"
 
-	exporter "github.com/bakins/php-fpm-exporter"
-	"github.com/spf13/cobra"
-	"go.uber.org/zap"
+        exporter "github.com/bakins/php-fpm-exporter"
+        "github.com/spf13/cobra"
+        "go.uber.org/zap"
 )
 
 var (
-	addr     *string
-	endpoint *string
+        addr     *string
+        endpoint *string
+        clientcert *string
+        clientkey *string
 )
 
 func serverCmd(cmd *cobra.Command, args []string) {
 
-	logger, err := exporter.NewLogger()
-	if err != nil {
-		panic(err)
-	}
+        logger, err := exporter.NewLogger()
+        if err != nil {
+                panic(err)
+        }
 
-	e, err := exporter.New(
-		exporter.SetAddress(*addr),
-		exporter.SetEndpoint(*endpoint),
-		exporter.SetLogger(logger),
-	)
+        e, err := exporter.New(
+                exporter.SetAddress(*addr),
+                exporter.SetEndpoint(*endpoint),
+                exporter.SetLogger(logger),
+                exporter.SetCerts(*clientcert, *clientkey),
+        )
 
-	if err != nil {
-		logger.Fatal("failed to create exporter", zap.Error(err))
-	}
+        if err != nil {
+                logger.Fatal("failed to create exporter", zap.Error(err))
+        }
 
-	if err := e.Run(); err != nil {
-		logger.Fatal("failed to run exporter", zap.Error(err))
-	}
+        if err := e.Run(); err != nil {
+                logger.Fatal("failed to run exporter", zap.Error(err))
+        }
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "php-fpm-exporter",
-	Short: "php-fpm metrics exporter",
-	Run:   serverCmd,
+        Use:   "php-fpm-exporter",
+        Short: "php-fpm metrics exporter",
+        Run:   serverCmd,
 }
 
 func main() {
-	addr = rootCmd.PersistentFlags().StringP("addr", "", "127.0.0.1:8080", "listen address for metrics handler")
-	endpoint = rootCmd.PersistentFlags().StringP("endpoint", "", "http://127.0.0.1:9000/status", "url for php-fpm status")
+        addr = rootCmd.PersistentFlags().StringP("addr", "", "127.0.0.1:8080", "listen address for metrics handler")
+        endpoint = rootCmd.PersistentFlags().StringP("endpoint", "", "http://127.0.0.1:9000/status", "url for php-fpm status")
+        clientcert = rootCmd.PersistentFlags().StringP("client.cert", "", "", "path to client cert file")
+        clientkey = rootCmd.PersistentFlags().StringP("client.key", "", "", "path to client key file")
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Printf("root command failed: %v", err)
-		os.Exit(-2)
-	}
+        if err := rootCmd.Execute(); err != nil {
+                fmt.Printf("root command failed: %v", err)
+                os.Exit(-2)
+        }
 }
--- collector.go
+++ collector.go
@@ -1,185 +1,210 @@
 package exporter
 
 import (
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"regexp"
-	"strconv"
 
-	"go.uber.org/zap"
+    "crypto/tls"
 
-	"github.com/pkg/errors"
-	"github.com/prometheus/client_golang/prometheus"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "regexp"
+    "strconv"
+
+    "go.uber.org/zap"
+
+    "github.com/pkg/errors"
+    "github.com/prometheus/client_golang/prometheus"
 )
 
 var (
-	statusLineRegexp = regexp.MustCompile(`(?m)^(.*):\s+(.*)$`)
+    statusLineRegexp = regexp.MustCompile(`(?m)^(.*):\s+(.*)$`)
 )
 
 type collector struct {
-	exporter           *Exporter
-	up                 *prometheus.Desc
-	acceptedConn       *prometheus.Desc
-	listenQueue        *prometheus.Desc
-	maxListenQueue     *prometheus.Desc
-	listenQueueLength  *prometheus.Desc
-	phpProcesses       *prometheus.Desc
-	maxActiveProcesses *prometheus.Desc
-	maxChildrenReached *prometheus.Desc
-	slowRequests       *prometheus.Desc
-	scrapeFailures     *prometheus.Desc
-	failureCount       int
+    exporter           *Exporter
+    up                 *prometheus.Desc
+    acceptedConn       *prometheus.Desc
+    listenQueue        *prometheus.Desc
+    maxListenQueue     *prometheus.Desc
+    listenQueueLength  *prometheus.Desc
+    phpProcesses       *prometheus.Desc
+    maxActiveProcesses *prometheus.Desc
+    maxChildrenReached *prometheus.Desc
+    slowRequests       *prometheus.Desc
+    scrapeFailures     *prometheus.Desc
+    failureCount       int
 }
 
 const metricsNamespace = "phpfpm"
 
 func newFuncMetric(metricName string, docString string, labels []string) *prometheus.Desc {
-	return prometheus.NewDesc(
-		prometheus.BuildFQName(metricsNamespace, "", metricName),
-		docString, labels, nil,
-	)
+    return prometheus.NewDesc(
+        prometheus.BuildFQName(metricsNamespace, "", metricName),
+        docString, labels, nil,
+    )
 }
 
 func (e *Exporter) newCollector() *collector {
-	return &collector{
-		exporter:           e,
-		up:                 newFuncMetric("up", "able to contact php-fpm", nil),
-		acceptedConn:       newFuncMetric("accepted_connections_total", "Total number of accepted connections", nil),
-		listenQueue:        newFuncMetric("listen_queue_connections", "Number of connections that have been initiated but not yet accepted", nil),
-		maxListenQueue:     newFuncMetric("listen_queue_max_connections", "Max number of connections the listen queue has reached since FPM start", nil),
-		listenQueueLength:  newFuncMetric("listen_queue_length_connections", "The length of the socket queue, dictating maximum number of pending connections", nil),
-		phpProcesses:       newFuncMetric("processes_total", "process count", []string{"state"}),
-		maxActiveProcesses: newFuncMetric("active_max_processes", "Maximum active process count", nil),
-		maxChildrenReached: newFuncMetric("max_children_reached_total", "Number of times the process limit has been reached", nil),
-		slowRequests:       newFuncMetric("slow_requests_total", "Number of requests that exceed request_slowlog_timeout", nil),
-		scrapeFailures:     newFuncMetric("scrape_failures_total", "Number of errors while scraping php_fpm", nil),
-	}
+    return &collector{
+        exporter:           e,
+        up:                 newFuncMetric("up", "able to contact php-fpm", nil),
+        acceptedConn:       newFuncMetric("accepted_connections_total", "Total number of accepted connections", nil),
+        listenQueue:        newFuncMetric("listen_queue_connections", "Number of connections that have been initiated but not yet accepted", nil),
+        maxListenQueue:     newFuncMetric("listen_queue_max_connections", "Max number of connections the listen queue has reached since FPM start", nil),
+        listenQueueLength:  newFuncMetric("listen_queue_length_connections", "The length of the socket queue, dictating maximum number of pending connections", nil),
+        phpProcesses:       newFuncMetric("processes_total", "process count", []string{"state"}),
+        maxActiveProcesses: newFuncMetric("active_max_processes", "Maximum active process count", nil),
+        maxChildrenReached: newFuncMetric("max_children_reached_total", "Number of times the process limit has been reached", nil),
+        slowRequests:       newFuncMetric("slow_requests_total", "Number of requests that exceed request_slowlog_timeout", nil),
+        scrapeFailures:     newFuncMetric("scrape_failures_total", "Number of errors while scraping php_fpm", nil),
+    }
 }
 
 func (c *collector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.up
-	ch <- c.scrapeFailures
-	ch <- c.acceptedConn
-	ch <- c.listenQueue
-	ch <- c.maxListenQueue
-	ch <- c.listenQueueLength
-	ch <- c.phpProcesses
-	ch <- c.maxActiveProcesses
-	ch <- c.maxChildrenReached
-	ch <- c.slowRequests
+    ch <- c.up
+    ch <- c.scrapeFailures
+    ch <- c.acceptedConn
+    ch <- c.listenQueue
+    ch <- c.maxListenQueue
+    ch <- c.listenQueueLength
+    ch <- c.phpProcesses
+    ch <- c.maxActiveProcesses
+    ch <- c.maxChildrenReached
+    ch <- c.slowRequests
 }
 
-func getData(u *url.URL) ([]byte, error) {
-	req := http.Request{
-		Method:     "GET",
-		URL:        u,
-		Proto:      "HTTP/1.1",
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header:     make(http.Header),
-		Host:       u.Host,
-	}
+func (c *collector) getData(u *url.URL) ([]byte, error) {
+    req := http.Request{
+        Method:     "GET",
+        URL:        u,
+        Proto:      "HTTP/1.1",
+        ProtoMajor: 1,
+        ProtoMinor: 1,
+        Header:     make(http.Header),
+        Host:       u.Host,
+    }
 
-	resp, err := http.DefaultClient.Do(&req)
-	if err != nil {
-		return nil, errors.Wrap(err, "HTTP request failed")
-	}
 
-	defer resp.Body.Close()
+    var client *http.Client
+    
+    if (c.exporter.clientcert != "") {
+        cert, err := tls.LoadX509KeyPair(c.exporter.clientcert, c.exporter.clientkey)
+        if err != nil {
+            return nil, errors.Wrap(err, "Could not load cert and/or key")
+        }
 
-	if resp.StatusCode != 200 {
-		return nil, errors.Errorf("unexpected HTTP status: %d", resp.StatusCode)
-	}
+        client = &http.Client{
+            Transport: &http.Transport{
+                TLSClientConfig: &tls.Config{
+                    Certificates: []tls.Certificate{cert},
+                    InsecureSkipVerify: false,
+                },
+            },
+        }
+    } else {
+        client = http.DefaultClient
+    }
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to read http body")
-	}
+    resp, err := client.Do(&req)
+    if err != nil {
+        return nil, errors.Wrap(err, "HTTP request failed")
+    }
 
-	return body, nil
+    defer resp.Body.Close()
+
+    if resp.StatusCode != 200 {
+        return nil, errors.Errorf("unexpected HTTP status: %d", resp.StatusCode)
+    }
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return nil, errors.Wrap(err, "failed to read http body")
+    }
+
+    return body, nil
 }
 
 func (c *collector) Collect(ch chan<- prometheus.Metric) {
-	up := 1.0
-	body, err := getData(c.exporter.endpoint)
-	if err != nil {
-		up = 0.0
-		c.exporter.logger.Error("failed to get php-fpm status", zap.Error(err))
-		c.failureCount++
-	}
-	ch <- prometheus.MustNewConstMetric(
-		c.up,
-		prometheus.GaugeValue,
-		up,
-	)
+    up := 1.0
+    body, err := c.getData(c.exporter.endpoint)
+    if err != nil {
+        up = 0.0
+        c.exporter.logger.Error("failed to get php-fpm status", zap.Error(err))
+        c.failureCount++
+    }
+    ch <- prometheus.MustNewConstMetric(
+        c.up,
+        prometheus.GaugeValue,
+        up,
+    )
 
-	ch <- prometheus.MustNewConstMetric(
-		c.scrapeFailures,
-		prometheus.CounterValue,
-		float64(c.failureCount),
-	)
+    ch <- prometheus.MustNewConstMetric(
+        c.scrapeFailures,
+        prometheus.CounterValue,
+        float64(c.failureCount),
+    )
 
-	if up == 0.0 {
-		return
-	}
+    if up == 0.0 {
+        return
+    }
 
-	matches := statusLineRegexp.FindAllStringSubmatch(string(body), -1)
-	for _, match := range matches {
-		key := match[1]
-		value, err := strconv.Atoi(match[2])
-		if err != nil {
-			continue
-		}
+    matches := statusLineRegexp.FindAllStringSubmatch(string(body), -1)
+    for _, match := range matches {
+        key := match[1]
+        value, err := strconv.Atoi(match[2])
+        if err != nil {
+            continue
+        }
 
-		var desc *prometheus.Desc
-		var valueType prometheus.ValueType
-		labels := []string{}
+        var desc *prometheus.Desc
+        var valueType prometheus.ValueType
+        labels := []string{}
 
-		switch key {
-		case "accepted conn":
-			desc = c.acceptedConn
-			valueType = prometheus.CounterValue
-		case "listen queue":
-			desc = c.listenQueue
-			valueType = prometheus.GaugeValue
-		case "max listen queue":
-			desc = c.maxListenQueue
-			valueType = prometheus.CounterValue
-		case "listen queue len":
-			desc = c.listenQueueLength
-			valueType = prometheus.GaugeValue
-		case "idle processes":
-			desc = c.phpProcesses
-			valueType = prometheus.GaugeValue
-			labels = append(labels, "idle")
-		case "active processes":
-			desc = c.phpProcesses
-			valueType = prometheus.GaugeValue
-			labels = append(labels, "active")
-		case "max active processes":
-			desc = c.maxActiveProcesses
-			valueType = prometheus.CounterValue
-		case "max children reached":
-			desc = c.maxChildrenReached
-			valueType = prometheus.CounterValue
-		case "slow requests":
-			desc = c.slowRequests
-			valueType = prometheus.CounterValue
-		default:
-			continue
-		}
+        switch key {
+        case "accepted conn":
+            desc = c.acceptedConn
+            valueType = prometheus.CounterValue
+        case "listen queue":
+            desc = c.listenQueue
+            valueType = prometheus.GaugeValue
+        case "max listen queue":
+            desc = c.maxListenQueue
+            valueType = prometheus.CounterValue
+        case "listen queue len":
+            desc = c.listenQueueLength
+            valueType = prometheus.GaugeValue
+        case "idle processes":
+            desc = c.phpProcesses
+            valueType = prometheus.GaugeValue
+            labels = append(labels, "idle")
+        case "active processes":
+            desc = c.phpProcesses
+            valueType = prometheus.GaugeValue
+            labels = append(labels, "active")
+        case "max active processes":
+            desc = c.maxActiveProcesses
+            valueType = prometheus.CounterValue
+        case "max children reached":
+            desc = c.maxChildrenReached
+            valueType = prometheus.CounterValue
+        case "slow requests":
+            desc = c.slowRequests
+            valueType = prometheus.CounterValue
+        default:
+            continue
+        }
 
-		m, err := prometheus.NewConstMetric(desc, valueType, float64(value), labels...)
-		if err != nil {
-			c.exporter.logger.Error(
-				"failed to create metrics",
-				zap.String("key", key),
-				zap.Error(err),
-			)
-			continue
-		}
+        m, err := prometheus.NewConstMetric(desc, valueType, float64(value), labels...)
+        if err != nil {
+            c.exporter.logger.Error(
+                "failed to create metrics",
+                zap.String("key", key),
+                zap.Error(err),
+            )
+            continue
+        }
 
-		ch <- m
-	}
+        ch <- m
+    }
 }
+
