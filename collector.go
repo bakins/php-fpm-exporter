218e0f232f80b11db25510739e0b668a9d709ae2
Benjamin Ullian <bnu@tumblr.com>
2021-01-29 10:21:33 -0500

    Add a status.timeout (env=STATUS_TIMEOUT) argument to prevent the exporter waiting forever on a hung php-fpm.
    Signed-off-by: Benjamin Ullian <bnu@tumblr.com>

--- collector.go
+++ collector.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"regexp"
 	"strconv"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
@@ -70,7 +71,8 @@ func (c *collector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.slowRequests
 }
 
-func getDataFastcgi(u *url.URL) ([]byte, error) {
+func (c *collector) getDataFastcgi() ([]byte, error) {
+	u := c.exporter.fcgiEndpoint
 	path := u.Path
 	host := u.Host
 
@@ -86,12 +88,16 @@ func getDataFastcgi(u *url.URL) ([]byte, error) {
 		"SCRIPT_NAME":     path,
 	}
 
-	fcgi, err := fcgiclient.Dial(u.Scheme, host)
+	fcgi, err := fcgiclient.DialTimeout(u.Scheme, host, c.exporter.statusTimeout)
 	if err != nil {
 		return nil, errors.Wrap(err, "fastcgi dial failed")
 	}
 
 	defer fcgi.Close()
+
+	if err = fcgi.SetTimeout(c.exporter.statusTimeout); err != nil {
+		return nil, errors.Wrap(err, "fastcgi SetTimeout failed")
+	}
 
 	resp, err := fcgi.Get(env)
 	if err != nil {
@@ -112,7 +118,8 @@ func getDataFastcgi(u *url.URL) ([]byte, error) {
 	return body, nil
 }
 
-func getDataHTTP(u *url.URL) ([]byte, error) {
+func (c *collector) getDataHTTP() ([]byte, error) {
+	u := c.exporter.endpoint
 	req := http.Request{
 		Method:     "GET",
 		URL:        u,
@@ -123,7 +130,7 @@ func getDataHTTP(u *url.URL) ([]byte, error) {
 		Host:       u.Host,
 	}
 
-	resp, err := http.DefaultClient.Do(&req)
+	resp, err := (&http.Client{Timeout: c.exporter.statusTimeout}).Do(&req)
 	if err != nil {
 		return nil, errors.Wrap(err, "HTTP request failed")
 	}
@@ -150,9 +157,9 @@ func (c *collector) Collect(ch chan<- prometheus.Metric) {
 	)
 
 	if c.exporter.fcgiEndpoint != nil && c.exporter.fcgiEndpoint.String() != "" {
-		body, err = getDataFastcgi(c.exporter.fcgiEndpoint)
+		body, err = c.getDataFastcgi()
 	} else {
-		body, err = getDataHTTP(c.exporter.endpoint)
+		body, err = c.getDataHTTP()
 	}
 
 	if err != nil {
